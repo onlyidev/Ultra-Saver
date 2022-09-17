405367e57621387c0fddcba70286c8216d72096a
Azuolas Ivoskus <ivoskailt@gmail.com>
2022-09-18 01:21:32 +0300

    Added very basic google OAuth, returns JWT token

--- ClientApp/src/AppRoutes.js
+++ ClientApp/src/AppRoutes.js
@@ -2,6 +2,7 @@ import { Counter } from "./components/Counter";
 import { FetchData } from "./components/FetchData";
 import { Home } from "./components/Home";
 import { HelloWorld } from "./components/HelloWorld";
+import Login from "./components/Login";
 
 const AppRoutes = [
   {
@@ -19,6 +20,10 @@ const AppRoutes = [
   {
     path: '/helloworld',
     element: <HelloWorld />
+  },
+  {
+    path: "/login",
+    element: <Login />
   }
 ];
 
---
+++ Controllers/LoginController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ultra_Saver.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class LoginController : ControllerBase
+{
+    
+}
