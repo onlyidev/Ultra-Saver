5064691e55c4b4febe00ea077d72728c00169205
onlyidev <liudas.kasper@gmail.com>
2022-09-17 16:54:03 +0300

    feature/setup-database Setup connection string with user secret

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using System.Data.Common;
+using System.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Ultra_Saver;
 
@@ -7,7 +9,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<SimpleDatabaseContext>(
-    o => o.UseNpgsql(builder.Configuration.GetConnectionString("db"))
+    o =>
+    {
+        var conectionString = builder.Configuration.GetConnectionString("db") + ";Password=" + builder.Configuration["DB:Password"];
+        o.UseNpgsql(conectionString);
+    }
 );
 
 var app = builder.Build();
@@ -28,6 +34,6 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller}/{action=Index}/{id?}");
 
-app.MapFallbackToFile("index.html");;
+app.MapFallbackToFile("index.html"); ;
 
 app.Run();
