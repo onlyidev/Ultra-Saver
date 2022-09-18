24cb6ba683838d32819ea230de8c63254c073fb0
Azuolas Ivoskus <ivoskailt@gmail.com>
2022-09-18 18:38:26 +0300

    Fixed/renamed backend route of login controller

--- ClientApp/src/setupProxy.js
+++ ClientApp/src/setupProxy.js
@@ -7,7 +7,7 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
 const context =  [
   "/weatherforecast",
   "/helloback",
-  "/login"
+  "/fetchlogindata"
 ];
 
 module.exports = function(app) {
