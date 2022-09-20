31613579e134fda9c42e9e186b6f4bb8597379df
Jurshenas <rokasjuren@gmail.com>
2022-09-20 12:25:05 +0300

    Added Log Out button Added User Data showing up on screen when Logged On

--- ClientApp/src/components/Login.js
+++ ClientApp/src/components/Login.js
@@ -1,6 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import jwt_decode from "jwt-decode";
 
 function Login() {
+
+  const [user, setUser] = useState({});
 
   useEffect(() => {
     /* global google */
@@ -17,15 +20,32 @@ function Login() {
 
 
   function HandleCallback(response) { // function called when user successfully logs in
+    document.getElementById("signInDiv").hidden = true;  // hides log in button when logged on
+    var userObject = jwt_decode(response.credential);    // decodes jwt token
+    setUser(userObject);  
     console.log("JWT ID Token: " + response.credential);
+    console.log(userObject);
+  }
+
+  function HandleLogOut(event) {   // handles logging out
+    setUser({});   // deletes user data
+    document.getElementById("signInDiv").hidden = false; //shows log in button when logged out
   }
 
   return (
     <div>
         <h1 id="tabelLabel" >Login</h1>
         <p>This component demonstrates the log in through google functionality.</p>
-        <div id="signInDiv"></div>
-      </div>
+        <div id="signInDiv"></div> 
+        { Object.keys(user).length !== 0 &&  // This div only shows up when user data is registered
+          <div>
+            <p>Signed in as: {user.name}</p>
+            <p>Email: {user.email}</p>
+            <button onClick={(e) => HandleLogOut(e)}>Sign Out</button>  
+          </div>
+        }    
+    </div>
+
   );
 }
 
