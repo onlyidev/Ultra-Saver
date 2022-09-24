3582fc702c6cfcc2479818a414a6d5a2c243ca95
onlyidev <liudas.kasper@gmail.com>
2022-09-24 17:49:41 +0300

    feature/connect-oauth-with-database Clean up + Make a user properties model + Create first route that requires authorization

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 using System.Data.Common;
 using System.Data.SqlClient;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Ultra_Saver;
 
@@ -7,14 +9,23 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<SimpleDatabaseContext>(
+builder.Services.AddControllers();
+
+// Add the AppDatabaseContext class as a service and connect it to the real PostgreSQL database
+builder.Services.AddDbContext<AppDatabaseContext>(
     o =>
     {
         var conectionString = builder.Configuration.GetConnectionString("db") + ";Password=" + builder.Configuration["DB:Password"];
         o.UseNpgsql(conectionString);
     }
 );
+
+// Add a service that can process the JWT token
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(jwt => jwt.UseGoogle(
+        clientId: "929531042172-l5h1hbegcb3qm6nkpg1r7m4aa6seb98n.apps.googleusercontent.com"
+    ));
+
 
 var app = builder.Build();
 
@@ -26,8 +37,12 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
+// app.UseStaticFiles();
 app.UseRouting();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
 
 
 app.MapControllerRoute(
