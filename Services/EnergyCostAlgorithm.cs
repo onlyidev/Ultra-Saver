6e5307ecadc0e00f58b47b576d56f7e3fee76d0a
Storas008 <naglis.kontautas04@gmail.com>
2022-10-05 12:42:36 +0300

    fixed getters and changed to PascelCase

--- Services/EnergyCostAlgorithm.cs
+++ Services/EnergyCostAlgorithm.cs
@@ -3,28 +3,23 @@ namespace Ultra_Saver;
 
 public class EnergyCostAlgorithm
 {
-    private static double totalEnergy = 0;
-    private static double totalEnergyCost = 0;
-    private static float costPerkWh = 0;
+    public double totalEnergy { get; set; } = 0;
+    public double totalEnergyCost { get; set;} = 0;
+    private float costPerkWh = 0;
 
     // powerScale is a proportion of power used for the specified time; scales 1 to 10; ex.: medium heat, 3/10 of power
-    private void electricPower (int appliancePower, short powerScale, int timeInMin){
+    private void ElectricPower (int appliancePower, short powerScale, int timeInMin){
         totalEnergy += appliancePower * powerScale * timeInMin / 600_000;
         totalEnergyCost = Math.Round (totalEnergy * costPerkWh, 2);
     }
 
-    private double gasPower (int appliancePower, short powerScale, int timeInMin){
+    private double GasPower (int appliancePower, short powerScale, int timeInMin){
         return 0;
     }
 
-    private double gasPowerCost (int appliancePower, short powerScale, int timeInMin, float costPerLitre){
+    private double GasPowerCost (int appliancePower, short powerScale, int timeInMin, float costPerLitre){
         return 0;
     }
 
-    // getters n setters:
-    public double getTotalEnergy(){ return totalEnergy; }
-
-    public double getTotalEnergyCost() { return totalEnergyCost ;}
-
-    public void setCostPerkWh(float cost){ costPerkWh = cost; }
+    public void SetCostPerkWh(float cost){ costPerkWh = cost; }
 }
